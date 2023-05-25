87386fd8322465bdc2e5d3fb38ab7e52877c1583
ir.__.si <cocoa9805@naver.com>
2023-05-25 18:34:39 +0900

    ✨Feat: #1 - viewModelType Protocol 정의

---
+++ Galapagos/Sources/Presenter/Common/ViewModelType.swift
@@ -0,0 +1,21 @@
+//
+//  ViewModelType.swift
+//  Galapagos
+//
+//  Created by 조용인 on 2023/05/25.
+//  Copyright © 2023 com.busyModernPeople. All rights reserved.
+//
+
+import RxSwift
+
+protocol ViewModelType: AnyObject{
+    associatedtype Input
+    associatedtype Output
+    
+    
+    // MARK: - Properties
+    var disposeBag: DisposeBag { get }
+    
+    // MARK: - Methods
+    func transform(input: Input) -> Output
+}
