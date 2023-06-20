bbdfb225804c48e9d3f01f10e94d2870889f0486
hyeun <81788774+shippingpark@users.noreply.github.com>
2023-06-20 18:19:05 +0900

    ✨ Feat: #34 - Coordinating 추가
    코디네이터 재사용 및 전환성 증대

---
+++ Galapagos/Sources/Presenter/TabBar/CommunityList/Post/Coordinator/DetailPostCoordinating.swift
@@ -0,0 +1,13 @@
+//
+//  DetailPostCoordinating.swift
+//  Galapagos
+//
+//  Created by 박혜운 on 2023/06/20.
+//  Copyright © 2023 com.busyModernPeople. All rights reserved.
+//
+
+import Foundation
+
+protocol DetailPostCoordinating {
+  func pushToDetailPost(postIdx: String)
+}
