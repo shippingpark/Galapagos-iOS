993a6486bc3d2b5ee911701df7153f5d6e18a025
hyeun <81788774+shippingpark@users.noreply.github.com>
2023-06-23 15:30:31 +0900

    🔨 Refactor: #36 ShadowView->RadiusBoxView로 명칭 변경

--- Galapagos/Sources/Presenter/TabBar/Main/Main/MainViewController.swift
+++ Galapagos/Sources/Presenter/TabBar/Main/Main/MainViewController.swift
@@ -15,7 +15,7 @@ final class MainViewController: BaseViewController {
   
   // MARK: - UI
   
-  private var shadowView = ShadowView()
+  private var shadowView = RadiusBoxView()
   private var button = UIButton().then {
     $0.backgroundColor = .darkGray
     $0.setTitle("펫 추가", for: .normal)
