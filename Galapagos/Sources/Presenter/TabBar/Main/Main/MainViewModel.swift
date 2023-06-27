215dd967fb59287d73fdc8b54b31d959098c18e3
hyeun <81788774+shippingpark@users.noreply.github.com>
2023-06-27 20:06:09 +0900

    ✨ Feat: #54 - 동물 등록 유무에 따른 화면 전환 로직

--- Galapagos/Sources/Presenter/TabBar/Main/Main/MainViewModel.swift
+++ Galapagos/Sources/Presenter/TabBar/Main/Main/MainViewModel.swift
@@ -14,10 +14,14 @@ import RxSwift
 final class MainViewModel: ViewModelType {
   struct Input {
     let addPetButtonTapped: Signal<Void>
+    let moveCommunityTapped: Signal<Void>
     let button2Tapped: Signal<Void>
   }
   
-  struct Output {}
+  struct Output {
+    let hasMainPet: Driver<Bool> //값을 가지고 오기 때문에 실제 적용 시 Bool 아닌 해당 Model 정보를 가져오는 게 좋을 듯 (isEmpty로 유무 판별)
+    let hasStarCommunity: Driver<Bool> //
+  }
   
   var disposeBag: DisposeBag = DisposeBag()
   weak var coordinator: MainCoordinator?
@@ -33,11 +37,21 @@ final class MainViewModel: ViewModelType {
       })
       .disposed(by: disposeBag)
     
+    input.moveCommunityTapped
+      .emit(onNext: { [weak self] _ in
+        self?.coordinator?.userActionState.accept(.moveCommunity)
+      })
+      .disposed(by: disposeBag)
+    
     input.button2Tapped
       .emit(onNext: { [weak self] _ in
         self?.coordinator?.userActionState.accept(.detailDiary)
       })
       .disposed(by: disposeBag)
-    return Output()
+    
+    return Output(
+      hasMainPet: Driver.just(false), //강제 입력
+      hasStarCommunity: Driver.just(false)
+    )
   }
 }
