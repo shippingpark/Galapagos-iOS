a5fbf4a3634cdc15ffc8a6a909e1710ab1ba55bf
hyeun <81788774+shippingpark@users.noreply.github.com>
2023-06-27 20:03:51 +0900

    💄 Ui: #49 - EmptyMainPetView 생성

---
+++ Galapagos/Sources/Presenter/TabBar/Main/Main/View/MainPetView.swift
@@ -0,0 +1,21 @@
+//
+//  MainAnimalView.swift
+//  Galapagos
+//
+//  Created by 박혜운 on 2023/06/27.
+//  Copyright © 2023 com.busyModernPeople. All rights reserved.
+//
+
+import UIKit
+
+class MainPetView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
