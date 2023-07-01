4b8eef9419f2561b59a7fc087228dafde8340299
ir.__.si <cocoa9805@naver.com>
2023-07-01 22:53:33 +0900

    ✨ Feat: #75 - Data레이어에 들어갈 섹션 나누고, 기본 설명 추가

---
+++ Galapagos/Sources/Data/EndPoint/TestEndpoint.swift
@@ -0,0 +1,13 @@
+//
+//  TestEndpoint.swift
+//  Galapagos
+//
+//  Created by 조용인 on 2023/07/01.
+//  Copyright © 2023 com.busyModernPeople. All rights reserved.
+//
+
+import Foundation
+
+/// 각 Repository별 최종 세팅값을 결정하는 곳 입니다.
+/// 예를 들어, BaseURL / HTTPMethod / Domain-Path / HTTPBody(Parameters)
+/// -> 쉽게 생각하면, Alamofire사용할 때 AF.()에 들어가는 내부 파라미터 결정하는 영역이라고 생각하면 됨
---
+++ Galapagos/Sources/Data/Repositories/TestRepository.swift
@@ -0,0 +1,13 @@
+//
+//  TestRepository.swift
+//  Galapagos
+//
+//  Created by 조용인 on 2023/07/01.
+//  Copyright © 2023 com.busyModernPeople. All rights reserved.
+//
+
+import Foundation
+
+/// 여기는, 실제 네트워크 통신을 진행하여, 1차 response를 받아오는 영역입니다
+/// 우리는 Firebase 사용 안하는 느낌이라, 자체 서버와의 통신이 대부분일 듯
+/// 필요에 의해 DTO로 Domain에 전달 할 수도 있고, response 상태 그대로를 반환 할 수도 있지요.
