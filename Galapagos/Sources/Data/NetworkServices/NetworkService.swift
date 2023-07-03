4ebebac7a6f6d08b5223b398a8544e8a3362d34d
ir.__.si <cocoa9805@naver.com>
2023-07-03 11:24:42 +0900

    ✨ Feat: #77 - NetworkService URLSession 세팅

---
+++ Galapagos/Sources/Data/NetworkServices/NetworkService.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkService.swift
+//  Galapagos
+//
+//  Created by 조용인 on 2023/07/03.
+//  Copyright © 2023 com.busyModernPeople. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+final class DefaultNetworkService: NetworkService {
+    
+    // MARK: - Properties
+    private let session: URLSession = .shared
+    
+    // MARK: - Methods
+
+    func request(_ endpoint: Endpoint) -> Observable<Data> {
+        guard let urlRequest = endpoint.toURLRequest() else { return .error(NetworkError.invalidURL) }
+        
+        return session.rx
+            .data(request: urlRequest)
+            .asObservable()
+    }
+}
