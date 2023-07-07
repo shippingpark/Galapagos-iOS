d8f93b8e066c2655fbf56f9a6b7784d9c5f12082
ir.__.si <cocoa9805@naver.com>
2023-07-07 15:34:58 +0900

    ✨ Feat: #88 - 비밀번호 확인 페이지 UI및 로직 구현 (no usecase)

--- Galapagos/Sources/Presenter/Auth/EmailSignUp/ScrollPages/PasswordCheckView.swift
+++ Galapagos/Sources/Presenter/Auth/EmailSignUp/ScrollPages/PasswordCheckView.swift
@@ -6,4 +6,265 @@
 //  Copyright © 2023 com.busyModernPeople. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SiriUIKit
+
+import RxSwift
+import RxCocoa
+
+import SnapKit
+
+final class PasswordCheckView: UIView {
+    // MARK: - UI
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "비밀번호를\n입력해주세요"
+        label.numberOfLines = 2
+        label.font = GalapagosFontFamily.Pretendard.bold.font(size: 28)
+        label.textColor = GalapagosAsset.blackHeading.color
+        return label
+    }()
+    
+    private lazy var passwordTextField: GalapagosTextField = {
+        let textFieldView = GalapagosTextField(
+            placeHolder: "비밀번호를 입력해주세요",
+            keyboardType: .default,
+            clearMode: .whileEditing
+        )
+        textFieldView.textField.isSecureTextEntry = true
+        return textFieldView
+    }()
+    
+    private lazy var passwordCheckTextField: GalapagosTextField = {
+        let textFieldView = GalapagosTextField(
+            placeHolder: "비밀번호를 재입력해주세요",
+            keyboardType: .default,
+            clearMode: .whileEditing
+        )
+        /// 비밀번호 확인은 우선 터치 막아두자
+        textFieldView.isUserInteractionEnabled = false
+        textFieldView.textField.isSecureTextEntry = true
+        return textFieldView
+    }()
+    
+    private lazy var passwordErrorStackView: UIStackView = {
+        let errorCell: [PasswordErrorCell] = [
+            PasswordErrorCell(errorMessage: "영문포함"),
+            PasswordErrorCell(errorMessage: "숫자포함"),
+            PasswordErrorCell(errorMessage: "특수문자포함"),
+            PasswordErrorCell(errorMessage: "8자~20자"),
+        ]
+        let stackView = UIStackView(arrangedSubviews: errorCell)
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private lazy var passwordCheckErrorCell: PasswordErrorCell = {
+        let errorCell = PasswordErrorCell(errorMessage: "비밀번호 일치")
+        return errorCell
+    }()
+    
+    
+    
+    // MARK: - Properties
+    
+    private let viewModel: EmailSignUpViewModel
+    private let disposeBag = DisposeBag()
+    
+    private let engValied: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    private let numValied: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    private let speValied: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    private let couValied: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    private let passwordResultCombined: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    
+    private let checkPasswordValied: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    
+    // MARK: - Initializers
+    
+    init(
+        frame: CGRect,
+        viewModel: EmailSignUpViewModel
+    ) {
+        self.viewModel = viewModel
+        super.init(frame: frame)
+        addViews()
+        setConstraints()
+        bind()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    
+    private func addViews(){
+        self.addSubviews([
+            titleLabel,
+            passwordTextField,
+            passwordErrorStackView,
+            passwordCheckTextField,
+            passwordCheckErrorCell
+        ])
+    }
+    
+    private func setConstraints(){
+        titleLabel.snp.makeConstraints { titleLabel in
+            titleLabel.top.equalToSuperview().offset(24)
+            titleLabel.leading.equalToSuperview().offset(24)
+        }
+        
+        passwordTextField.snp.makeConstraints { passwordTextField in
+            passwordTextField.top.equalTo(titleLabel.snp.bottom).offset(40)
+            passwordTextField.leading.equalToSuperview().offset(24)
+            passwordTextField.trailing.equalToSuperview().offset(-24)
+            passwordTextField.height.equalTo(68)
+        }
+        
+        passwordErrorStackView.snp.makeConstraints { passwordErrorStackView in
+            passwordErrorStackView.top.equalTo(passwordTextField.snp.bottom).offset(6)
+            passwordErrorStackView.leading.equalToSuperview().offset(24)
+        }
+        
+        passwordCheckTextField.snp.makeConstraints { passwordCheckTextField in
+            passwordCheckTextField.top.equalTo(passwordErrorStackView.snp.bottom).offset(30)
+            passwordCheckTextField.leading.equalToSuperview().offset(24)
+            passwordCheckTextField.trailing.equalToSuperview().offset(-24)
+            passwordCheckTextField.height.equalTo(68)
+        }
+        
+        passwordCheckErrorCell.snp.makeConstraints { passwordCheckErrorCell in
+            passwordCheckErrorCell.top.equalTo(passwordCheckTextField.snp.bottom).offset(6)
+            passwordCheckErrorCell.leading.equalToSuperview().offset(24)
+        }
+        
+    }
+    
+    // TODO: 원래는 Usecase에 들어가야할 애들인데, 일단은 여기에 다 박아둠
+    
+    private func bind(){
+        passwordTextField.textField.rx.text.orEmpty
+            .asDriver()
+            .distinctUntilChanged()
+            .drive(onNext: { [weak self] text in
+                guard let self = self else { return }
+                self.checkPasswordEnglishValidation(password: text)
+                self.checkPasswordNumberValidation(password: text)
+                self.checkPasswordSpecialCharacterValidation(password: text)
+                self.checkPasswordLengthValidation(password: text)
+            })
+            .disposed(by: disposeBag)
+        
+        Observable.combineLatest(engValied, numValied, speValied, couValied)
+            .asDriver(onErrorJustReturn: (false, false, false, false))
+            .map { $0.0 && $0.1 && $0.2 && $0.3 }
+            .drive(onNext: { [weak self] isValidated in
+                guard let self = self else { return }
+                self.passwordResultCombined.onNext(isValidated)
+            })
+            .disposed(by: disposeBag)
+        
+        passwordResultCombined
+            .asDriver(onErrorJustReturn: false)
+            .drive(onNext: { [weak self] isValidated in
+                guard let self = self else { return }
+                self.passwordCheckTextField.isUserInteractionEnabled = isValidated
+            })
+            .disposed(by: disposeBag)
+        
+        passwordCheckTextField.textField.rx.text.orEmpty
+            .asDriver()
+            .debug()
+            .drive(onNext: { [weak self] text in
+                guard let self = self else { return }
+                text == passwordTextField.textField.text! && !text.isEmpty ? self.checkPasswordValied.onNext(true) : self.checkPasswordValied.onNext(false)
+            })
+            .disposed(by: disposeBag)
+        
+        checkPasswordValied
+            .asDriver(onErrorJustReturn: false)
+            .drive(onNext: { [weak self] isValidated in
+                guard let self = self else { return }
+                isValidated ? self.passwordCheckErrorCell.changeState(state: .success) : self.passwordCheckErrorCell.changeState(state: .error)
+                isValidated ? self.viewModel.readyForNextButton.accept(true) : self.viewModel.readyForNextButton.accept(false)
+            })
+            .disposed(by: disposeBag)
+        
+    }
+    
+    //영어가 들어있는지를 확인해서 Observable<Bool>타입으로 반환해주는 함수
+    private func checkPasswordEnglishValidation(password: String) {
+        let passwordRegex = ".*[A-Za-z]+.*"
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+        Observable.just(passwordTest.evaluate(with: password))
+            .asDriver(onErrorJustReturn: false)
+            .drive(onNext: { [weak self] isValidated in
+                guard let self = self else { return }
+                guard let cell = passwordErrorStackView.subviews[0] as? PasswordErrorCell else { return }
+                isValidated ? cell.changeState(state: .success) : cell.changeState(state: .error)
+                isValidated ? self.engValied.onNext(true) : self.engValied.onNext(false)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    //숫자가 들어있는지를 확인해서 Observable<Bool>타입으로 반환해주는 함수
+    private func checkPasswordNumberValidation(password: String) {
+        let passwordRegex = ".*[0-9]+.*"
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+        Observable.just(passwordTest.evaluate(with: password))
+            .asDriver(onErrorJustReturn: false)
+            .drive(onNext: { [weak self] isValidated in
+                guard let self = self else { return }
+                guard let cell = passwordErrorStackView.subviews[1] as? PasswordErrorCell else { return }
+                isValidated ? cell.changeState(state: .success) : cell.changeState(state: .error)
+                isValidated ? self.numValied.onNext(true) : self.numValied.onNext(false)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    //특수문자가 들어있는지를 확인해서 Observable<Bool>타입으로 반환해주는 함수
+    private func checkPasswordSpecialCharacterValidation(password: String) {
+        let passwordRegex = ".*[!@#$%^&*()_+{}:<>?]+.*"
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+        Observable.just(passwordTest.evaluate(with: password))
+            .asDriver(onErrorJustReturn: false)
+            .drive(onNext: { [weak self] isValidated in
+                guard let self = self else { return }
+                guard let cell = passwordErrorStackView.subviews[2] as? PasswordErrorCell else { return }
+                isValidated ? cell.changeState(state: .success) : cell.changeState(state: .error)
+                isValidated ? self.speValied.onNext(true) : self.speValied.onNext(false)
+                
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    //비밀번호가 8자~20자인지 확인해서 Observable<Bool>타입으로 반환해주는 함수
+    private func checkPasswordLengthValidation(password: String) {
+        Observable.just(password.count >= 8 && password.count <= 20)
+            .asDriver(onErrorJustReturn: false)
+            .drive(onNext: { [weak self] isValidated in
+                guard let self = self else { return }
+                guard let cell = passwordErrorStackView.subviews[3] as? PasswordErrorCell else { return }
+                isValidated ? cell.changeState(state: .success) : cell.changeState(state: .error)
+                isValidated ? self.couValied.onNext(true) : self.couValied.onNext(false)
+                
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    
+    // TODO: 각 Error의 state를 combineLatest해서 isValied면 checkPassword Active하게
+    private func checkPasswordValidation() {
+        
+    }
+    
+}
+
+// MARK: - Extension
+extension PasswordCheckView {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.endEditing(true)
+    }
+}
